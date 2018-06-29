8e6fdfd2804b318222c5bfef5e33ccd677fe9fce
Kyle Ranger <zranger2345@gmail.com>
2018-06-29 14:23:08 -0500

    Created the home page and set up all handlers in app

--- app.go
+++ app.go
@@ -2,6 +2,39 @@
 // the REST_game api.
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8081",
+	}
+	http.HandleFunc("/", root)
+	http.HandleFunc("/contribute", contribute)
+	http.HandleFunc("/search/title", searchByTitle)
+	http.HandleFunc("/search/developer", searchByDeveloper)
+	http.HandleFunc("/search/rating", searchByRating)
+	server.ListenAndServe()
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Root")
+}
+
+func contribute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Contribute")
+}
+
+func searchByTitle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Title")
+}
+
+func searchByDeveloper(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Developer")
+}
+
+func searchByRating(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Rating")
 }
