d0c1e44f566624c969f966c25c63d4e01b0cec10
Kyle Ranger <zranger2345@gmail.com>
2018-06-30 15:45:14 -0500

    Created the contribute page and the app now can send game data to the API

--- app.go
+++ app.go
@@ -3,10 +3,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 )
+
+type Game struct {
+	Title     string `json:"title"`
+	Developer string `json:"developer"`
+	Rating    string `json:"rating"`
+}
 
 func main() {
 	server := http.Server{
@@ -14,6 +22,7 @@ func main() {
 	}
 	http.HandleFunc("/", root)
 	http.HandleFunc("/contribute", contribute)
+	http.HandleFunc("/submit", submitGameToAPI)
 	http.HandleFunc("/search/title", searchByTitle)
 	http.HandleFunc("/search/developer", searchByDeveloper)
 	http.HandleFunc("/search/rating", searchByRating)
@@ -24,14 +33,34 @@ func main() {
 func root(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("homepage.html")
 	if err != nil {
-		fmt.Println("Unable to pare homepage.html", err.Error())
+		fmt.Println("Unable to parse homepage.html", err.Error())
 	}
 	t.Execute(w, nil)
 }
 
-// contribute serves up the contribute page
+// contribute serves up the contribute page so the client can
+// add to the game DB
 func contribute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Contribute")
+	t, err := template.ParseFiles("contribute.html")
+	if err != nil {
+		fmt.Println("Unable to parse contribute.html")
+	}
+	t.Execute(w, nil)
+}
+
+// submitGameToAPI will extract the client provided game data
+// from the request, convert it into json and send to the game API
+func submitGameToAPI(w http.ResponseWriter, r *http.Request) {
+	game := Game{r.PostFormValue("title"), r.PostFormValue("developer"), r.PostFormValue("rating")}
+	jsonData, err := json.Marshal(game)
+	if err != nil {
+		fmt.Println("Error creating json")
+	}
+	_, err = http.Post("http://localhost:8080/gameAPI/add", "application/json",
+		bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Unable to connect to the game API")
+	}
 }
 
 // searchByTitle will display the data on the specified game
