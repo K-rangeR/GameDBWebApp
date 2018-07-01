e4498328d4b591c8f8bf3ebada533d83796c3704
Kyle Ranger <zranger2345@gmail.com>
2018-07-01 16:08:01 -0500

    Client can now search by developer. Note that the output is still not good

--- app.go
+++ app.go
@@ -17,6 +17,11 @@ type Game struct {
 	Rating    string `json:"rating"`
 }
 
+// GameList represents a list of games
+type GameList struct {
+	games []Game `json:"games"`
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8081",
@@ -35,7 +40,7 @@ func main() {
 
 // root will serve up the home page
 func root(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("homepage.html")
+	t, err := template.ParseFiles("htmlpages/homepage.html")
 	if err != nil {
 		fmt.Println("Unable to parse homepage.html", err.Error())
 	}
@@ -45,7 +50,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 // contribute serves up the contribute page so the client can
 // add to the game DB
 func contribute(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("contribute.html")
+	t, err := template.ParseFiles("htmlpages/contribute.html")
 	if err != nil {
 		fmt.Println("Unable to parse contribute.html")
 	}
@@ -69,16 +74,24 @@ func submitGameToAPI(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// serveTitleInput will serve up the html page that allows the client
+// search the game DB by the games title
 func serveTitleInput(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("searchByTitle.html")
+	t, err := template.ParseFiles("htmlpages/searchByTitle.html")
 	if err != nil {
 		fmt.Println("Unable to parse searchByTitle.html")
 	}
 	t.Execute(w, nil)
 }
 
+// serveDeveloperInput will serve up the html page that allows the client
+// to search for all games created by the specified developer
 func serveDeveloperInput(w http.ResponseWriter, r *http.Request) {
-
+	t, err := template.ParseFiles("htmlpages/searchByDeveloper.html")
+	if err != nil {
+		fmt.Println("Unable to parse searchByDeveloper.html")
+	}
+	t.Execute(w, nil)
 }
 
 func serveRatingInput(w http.ResponseWriter, r *http.Request) {
@@ -91,6 +104,7 @@ func searchByTitle(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://localhost:8080/gameAPI/" + title) // make this better
 	if err != nil {
 		fmt.Fprintln(w, "Could not connect to the DB, try again later")
+		return
 	}
 	var game Game
 	dataLen := resp.ContentLength
@@ -103,7 +117,21 @@ func searchByTitle(w http.ResponseWriter, r *http.Request) {
 // searchByDeveloper will display a list of games made by the
 // specified developer
 func searchByDeveloper(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Developer")
+	developer := r.FormValue("developer")
+	resp, err := http.Get("http://localhost:8080/gameAPI/developer/" + developer)
+	if err != nil {
+		fmt.Fprintln(w, "Could not connect to the DB, try again later")
+		return
+	}
+	var games []Game
+	dataLen := resp.ContentLength
+	jsonData := make([]byte, dataLen)
+	resp.Body.Read(jsonData)
+	json.Unmarshal(jsonData, &games)
+	// Add better output
+	for _, game := range games {
+		fmt.Fprintln(w, game)
+	}
 }
 
 // searchByRating will display a list of all games with the
