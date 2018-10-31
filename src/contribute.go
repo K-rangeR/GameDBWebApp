7d535513179f80dbb5f0b1e3e823abdcb6136c9c
Kyle Ranger <zranger2345@gmail.com>
2018-10-31 08:26:57 -0500

    Improve doc and error handling in contribute.go

--- src/contribute.go
+++ src/contribute.go
@@ -14,22 +14,25 @@ const (
 )
 
 // contribute serves up the contribute page so the client can
-// add to the game DB
+// add a game to the game DB
 func contribute(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(contributePagePath, menuPagePath)
 	if err != nil {
-		fmt.Println("Unable to parse contribute.html")
+		fmt.Println("Unable to parse contribute.html:", err)
+		return
 	}
 	t.ExecuteTemplate(w, "contribute", nil)
 }
 
 // submitGameToAPI will extract the client provided game data
-// from the request, convert it into json and send to the game API
+// from the request, convert it into json and send it to the game API
 func submitGameToAPI(w http.ResponseWriter, r *http.Request) {
 	game := Game{r.PostFormValue("title"), r.PostFormValue("developer"), r.PostFormValue("rating")}
 	jsonData, err := json.Marshal(game)
 	if err != nil {
-		fmt.Println("Error creating json")
+		fmt.Println("Error creating json:", err)
+		fmt.Fprintln(w, "Looks like there was a problem, try again later")
+		return
 	}
 
 	resp, err := http.Post(apiAddEndPoint, "application/json", bytes.NewBuffer(jsonData))
